714af541c719ab79c4bcec5827c33c5a92890dde
Syed Ali Qasim Khundmiri <92255662+aliqasim54321@users.noreply.github.com>
2025-01-16 14:52:25 -0500

    Update config.js

--- doctor-appointment-final/doctor-appointment/frontend/src/config.js
+++ doctor-appointment-final/doctor-appointment/frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://hospital-management-32wq.onrender.com";
+export const BASE_URL = "https://hospital-management-32wq.onrender.com/api/v1";
 export const token = localStorage.getItem("token");
